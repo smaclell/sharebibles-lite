10d7110ee07910defe16325123cbf814e429cf39
Scott MacLellan <scott.maclellan@vidyard.com>
2017-09-04 19:58:39 -0400

    Thunks yo

--- app/App.js
+++ app/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import Navigation from './nav';
 import reducer from './reducers';
 import initialState from './reducers/initial-state';
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+  reducer,
+  initialState,
+  applyMiddleware(thunk),
+);
 
 const App = () => (
   <Provider store={store}>

3769dc9a359517c037f5e2cf8d57cb49540c181d
Scott MacLellan <scott.maclellan@vidyard.com>
2017-10-07 22:15:50 -0400

    Dropping uploaded for now

--- components/Location.js
+++ components/Location.js
@@ -10,14 +10,14 @@ import moment from 'moment';
 import styles from '../styles/locations';
 import fonts from '../styles/fonts';
 
-const Location = ({ name, created, uploaded, navigate }) => (
+const Location = ({ name, created, navigate }) => (
   <TouchableOpacity onPressOut={() => navigate()}>
     <View style={styles.item}>
       <Image source={require('../assets/logo/logo.png')} style={styles.item_image} />
       <View style={{ flex: 1, minWidth: 100, marginRight: 35 }}>
         <Text style={{ fontSize: fonts.large, fontWeight: 'bold' }}>{ name }</Text>
         <Text>Last visited {moment.utc(created).clone().local().fromNow()}</Text>
-        <Text style={{ fontSize: fonts.normal, color: uploaded ? 'blue' : undefined }}>{ uploaded ? 'Uploaded' : 'Pending' }</Text>
+        <Text style={{ fontSize: fonts.normal, color: 'blue' }}>{ uploaded ? 'Uploaded' : 'Pending' }</Text>
       </View>
       <View style={styles.circle} />
     </View>
@@ -28,7 +28,6 @@ Location.propTypes = {
   created: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   navigate: PropTypes.func.isRequired,
-  uploaded: PropTypes.bool.isRequired,
 };
 
 export default Location;

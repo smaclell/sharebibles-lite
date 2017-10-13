9a59a2d51c779c536d30b31f008da46ac335639d
Scott MacLellan <scott.maclellan@vidyard.com>
2017-10-13 00:29:25 -0400

    New callout component

---
+++ components/PinCallout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import styles from '../styles/map-screen';
+import fonts from '../styles/fonts';
+
+const Callout = ({ created, navigate, visits }) => (
+  <View style={styles.callout_container}>
+    <Image source={require('../assets/logo/logo.png')} style={styles.callout_image} />
+    <View style={{ marginBottom: 5 }}>
+      <Text style={{ fontSize: fonts.normal }}>Last visited {moment.utc(created).clone().local().fromNow()}</Text>
+      <Text style={{ fontSize: fonts.normal }}> {visits} Visit(s) </Text>
+    </View>
+    <Text onPress={navigate} style={{ fontSize: fonts.small, color: 'red', fontWeight: 'bold' }}>
+      Tap to Visit
+    </Text>
+  </View>
+);
+
+Callout.propTypes = {
+  created: PropTypes.number.isRequired,
+  navigate: PropTypes.func.isRequired,
+  visits: PropTypes.number.isRequired,
+};
+
+export default Callout;

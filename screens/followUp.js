3dc9acc0578ee4b521cf1d19b588325cb78fcceb
Scott MacLellan <scott.maclellan@vidyard.com>
2017-10-21 18:21:23 -0400

    Styling change to follow up

--- screens/followUp.js
+++ screens/followUp.js
@@ -15,101 +15,101 @@ import User from '../components/User';
 import styles from '../styles/followUp';
 
 class FollowUp extends React.Component {
-    static navigationOptions = {
-      header: null,
-      tabBarVisible: false,
-    }
+  static navigationOptions = {
+    header: null,
+    tabBarVisible: false,
+  }
 
-    static propTypes = { // Sorted Alphabetically
-      createVisit: PropTypes.func.isRequired,
-      navigation: PropTypes.object.isRequired,
-      tags: PropTypes.array.isRequired,
-      user: PropTypes.object.isRequired,
-    }
+  static propTypes = { // Sorted Alphabetically
+    createVisit: PropTypes.func.isRequired,
+    navigation: PropTypes.object.isRequired,
+    tags: PropTypes.array.isRequired,
+    user: PropTypes.object.isRequired,
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        notes: null,
-        tags: {},
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: null,
+      tags: {},
+    };
 
-      this.showTag = this.showTag.bind(this);
-    }
+    this.showTag = this.showTag.bind(this);
+  }
 
-    showTag(tag) {
-      return (
-        <Switch
-          key={tag.key}
-          onChange={enabled => this.updateTag(tag.key, enabled)}
-          value={!!this.state.tags[tag.key]}
-        >
-          {tag.label}
-        </Switch>
-      );
-    }
+  showTag(tag) {
+    return (
+      <Switch
+        key={tag.key}
+        onChange={enabled => this.updateTag(tag.key, enabled)}
+        value={!!this.state.tags[tag.key]}
+      >
+        {tag.label}
+      </Switch>
+    );
+  }
 
-    update() {
-      const { params: { locationKey } } = this.props.navigation.state;
-      const { notes, tags } = this.state;
+  update() {
+    const { params: { locationKey } } = this.props.navigation.state;
+    const { notes, tags } = this.state;
 
-      this.props.createVisit({ locationKey, notes, tags });
-      this.props.navigation.goBack();
-    }
+    this.props.createVisit({ locationKey, notes, tags });
+    this.props.navigation.goBack();
+  }
 
-    updateTag(tagKey, enabled) {
-      this.setState(p => ({
-        ...p,
-        tags: {
-          ...p.tags,
-          [tagKey]: enabled,
-        },
-      }));
-    }
+  updateTag(tagKey, enabled) {
+    this.setState(p => ({
+      ...p,
+      tags: {
+        ...p.tags,
+        [tagKey]: enabled,
+      },
+    }));
+  }
 
-    render() {
-      return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
 
-          <View style={styles.users_container}>
-            <View style={styles.container_heading}>
-              <Text style={styles.container_heading_text}> 1 </Text>
-            </View>
-
-            <User {...this.props.user} />
+        <View style={styles.users_container}>
+          <View style={styles.container_heading}>
+            <Text style={styles.container_heading_text}> 1 </Text>
           </View>
 
-          <View style={styles.tags_container}>
-            <View style={styles.container_heading}>
-              <Text style={styles.container_heading_text}> 2 </Text>
-            </View>
+          <User {...this.props.user} />
+        </View>
 
-            <View style={{ flex: 1, margin: 10 }}>
-              { this.props.tags.map(this.showTag) }
-            </View>
+        <View style={styles.tags_container}>
+          <View style={styles.container_heading}>
+            <Text style={styles.container_heading_text}> 2 </Text>
           </View>
 
-          <View style={styles.notes_container}>
-            <View style={styles.container_heading}>
-              <Text style={styles.container_heading_text}> 3 </Text>
-            </View>
-            <TextInput
-              style={styles.note_input}
-              placeholder="Add notes..."
-              returnKeyType="next"
-              multiline
-              onChangeText={notes => this.setState(p => ({ ...p, notes }))}
-            />
+          <View style={{ flex: 1, margin: 10 }}>
+            { this.props.tags.map(this.showTag) }
           </View>
+        </View>
 
-          <View style={styles.actions_container}>
-            <Button onClick={() => this.update()}>UPDATE</Button>
-            <Button onClick={() => this.props.navigation.goBack()}>CANCEL</Button>
+        <View style={styles.notes_container}>
+          <View style={styles.container_heading}>
+            <Text style={styles.container_heading_text}> 3 </Text>
           </View>
+          <TextInput
+            style={styles.note_input}
+            placeholder="Add notes..."
+            returnKeyType="next"
+            multiline
+            onChangeText={notes => this.setState(p => ({ ...p, notes }))}
+          />
+        </View>
 
-        </KeyboardAvoidingView>
-      );
-    }
+        <View style={styles.actions_container}>
+          <Button onClick={() => this.update()}>UPDATE</Button>
+          <Button onClick={() => this.props.navigation.goBack()}>CANCEL</Button>
+        </View>
+
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const mapStateToProps = state => ({

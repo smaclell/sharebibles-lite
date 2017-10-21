33e02be1fb438bab922e3c1676fd801208fd5a36
Pim Vermeyden <pim.vermeyden@vidyard.com>
2017-10-21 15:45:07 -0400

    add color and rank

--- reducers/statuses.js
+++ reducers/statuses.js
@@ -1,9 +1,9 @@
 // A hardcoded list of interactions for each location and visit
 const statuses =[
-  { key: 'accepted', label: 'Accepted', icon: 'check'},
-  { key: 'delivered', label: 'Delivered', icon: 'book'},
-  { key: 'not-home', label: 'Not Home', icon: 'repeat'},
-  { key: 'rejected', label: 'Rejected', icon: 'close'},
+  { key: 'accepted', label: 'Accepted', icon: 'check', colorPin: 'green', rank: 0 },
+  { key: 'delivered', label: 'Delivered', icon: 'book', colorPin: 'blue', rank: 1 },
+  { key: 'not-home', label: 'Not Home', icon: 'repeat', colorPin: 'grey', rank: 2 },
+  { key: 'rejected', label: 'Rejected', icon: 'close', colorPin: 'red', rank: 3 },
 ];
 
 function reducer(state = statuses) {

e7f6fcbf03c77b410ec6c1c60e880954f5346632
Scott MacLellan <scott.maclellan@vidyard.com>
2017-11-25 15:22:28 -0500

    Pulling out the home nav

--- nav/index.js
+++ nav/index.js
@@ -1,72 +1,8 @@
-/* globals __DEV__ */
-/* eslint react/prop-types: 0 */
-import React from 'react';
-import { Animated, Easing, TouchableOpacity } from 'react-native';
-import { StackNavigator, DrawerNavigator } from 'react-navigation';
-import { FontAwesome } from '@expo/vector-icons';
+import { Animated, Easing } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-import I18n from '../assets/i18n/i18n';
-import colours from '../styles/colours';
-
-import Dev from '../screens/dev';
-import FollowUp from '../screens/followUp';
-import Tabs from './tabs';
+import Home from './home';
 import Login from './login';
-
-const createGear = navigation => (
-  <TouchableOpacity style={{ paddingLeft: 10, paddingRight: 10 }} onPress={() => navigation.navigate('DrawerOpen')}>
-    <FontAwesome size={24} name="gear" color={colours.text} />
-  </TouchableOpacity>
-);
-
-const drawerScreens = {
-  Tabs: {
-    screen: Tabs,
-    navigationOptions: ({ navigation }) => ({
-      title: I18n.t('title/share_bibles'),
-      gesturesEnabled: false,
-      headerLeft: createGear(navigation),
-    }),
-  },
-};
-
-if (__DEV__) {
-  drawerScreens.Dev = {
-    screen: Dev,
-    navigationOptions: {
-      title: 'Developer Testing Screen',
-      drawerLabel: 'Dev',
-    },
-  };
-}
-
-const drawer = new DrawerNavigator(drawerScreens, {
-  headerMode: 'float',
-  drawerLockMode: 'locked-closed',
-  navigationOptions: {
-    headerStyle: { backgroundColor: colours.white },
-    headerTintColor: colours.text,
-  },
-});
-
-const drawerStack = new StackNavigator({
-  Drawer: {
-    screen: drawer,
-  },
-  FollowUp: {
-    screen: FollowUp,
-    navigationOptions: {
-      title: I18n.t('components/follow_up'),
-      tabBarVisible: false,
-      gesturesEnabled: false,
-    },
-  },
-}, {
-  headerMode: 'float',
-  navigationOptions: {
-    gesturesEnabled: false,
-  },
-});
 
 // https://github.com/react-community/react-navigation/issues/1254
 const noTransitionConfig = () => ({
@@ -79,7 +15,7 @@ const noTransitionConfig = () => ({
 
 export default new StackNavigator({
   Login: { screen: Login },
-  Home: { screen: drawerStack },
+  Home: { screen: Home },
 }, {
   headerMode: 'none',
   navigationOptions: {

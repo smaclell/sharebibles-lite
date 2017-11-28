e7a55a64c014de0c49d67d47e334e9170b04938e
Scott MacLellan <scott.maclellan@vidyard.com>
2017-11-27 22:56:17 -0500

    Fix status colour

--- reducers/statuses.js
+++ reducers/statuses.js
@@ -2,7 +2,7 @@
 const statuses = [
   // Home
   { key: 'accepted', label: 'status/accepted', icon: 'book', iconFamily: 'font-awesome', pinColor: 'green', group: 'home', success: true },
-  { key: 'need', label: 'status/need', icon: 'exclamation-circle', iconFamily: 'font-awesome', pinColor: 'green', group: 'home', success: false },
+  { key: 'need', label: 'status/need', icon: 'exclamation-circle', iconFamily: 'font-awesome', pinColor: 'grey', group: 'home', success: false },
   { key: 'have', label: 'status/have', icon: 'check-circle', iconFamily: 'font-awesome', pinColor: 'green', group: 'home', success: true },
   { key: 'rejected', label: 'status/rejected', icon: 'circle-with-cross', iconFamily: 'entypo', pinColor: 'grey', group: 'home', success: false },
 

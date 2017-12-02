532afe0734a1de9ac0aee445f6bd494e8166343e
Scott MacLellan <scott.maclellan@vidyard.com>
2017-12-01 23:17:13 -0500

    Lint

--- apis/index.js
+++ apis/index.js
@@ -101,7 +101,9 @@ export async function signUp(name, email, password, accessCode) {
 
   const hashCode = hashAccessCode(accessCode);
 
-  const { uid: userKey } = await persist(firebase.auth().createUserWithEmailAndPassword(email, password));
+  const { uid: userKey } = await persist(
+    firebase.auth().createUserWithEmailAndPassword(email, password),
+  );
 
   await firebase.database().ref(`accessCodes/${hashCode}/userKey`).set(userKey);
 

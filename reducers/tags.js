13685f9f102726725e6d509d303130e931a9e6e0
Pim Vermeyden <pim.vermeyden@vidyard.com>
2017-12-10 17:07:09 -0500

    shared

--- reducers/tags.js
+++ reducers/tags.js
@@ -1,42 +1,43 @@
 // A hardcoded list of interactions for each location and visit
+const shared = ['accepted', 'need', 'have', 'rejected', 'delivered'];
 const tags = {
   initial: [
     {
       key: 'cannot_read',
       label: 'tag/initial/cannot_read',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
     {
       key: 'christian',
       label: 'tag/initial/christian',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
   ],
   followUp: [
     {
       key: 'prayer',
       label: 'tag/followup/prayer',
-      statuses: ['accepted', 'need', 'have', 'rejected', 'delivered', 'unfinished'],
+      statuses: [...shared, 'unfinished'],
     },
     {
       key: 'read_bible',
       label: 'tag/followup/read_bible',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
     {
       key: 'understood_gospel',
       label: 'tag/followup/understood_gospel',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
     {
       key: 'joined_discipleship_group',
       label: 'tag/followup/joined_discipleship_group',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
     {
       key: 'received_baptism',
       label: 'tag/followup/received_baptism',
-      statuses: ['accepted', 'need', 'have', 'rejected'],
+      statuses: shared,
     },
   ],
 };

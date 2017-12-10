9bedfbee811b9506faec3fbda4608262a9856c77
Pim Vermeyden <pim.vermeyden@vidyard.com>
2017-12-10 17:02:04 -0500

    keep it optional, but use the default

--- components/ResourceCounter.js
+++ components/ResourceCounter.js
@@ -58,7 +58,7 @@ class ResourceCounter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: props.count || 0,
+      count: props.count,
     };
 
     this.handleCountChanged = this.handleCountChanged.bind(this);

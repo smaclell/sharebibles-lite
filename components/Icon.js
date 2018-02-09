fb8d2e144fd397c1f73a86bf0825ba31d5081ff3
jfangrad <justin.fangrad@vidyard.com>
2018-02-09 18:15:21 -0500

    Missed Semicolon

--- components/Icon.js
+++ components/Icon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ViewPropTypes } from 'react-native'
+import { ViewPropTypes } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
 
 const families = {

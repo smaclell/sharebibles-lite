14e7df174d9167b159f102b1dbda366ffa328967
Scott MacLellan <scott.maclellan@vidyard.com>
2018-04-03 21:12:40 -0400

    Splitting out refetch

--- actions/authentication.js
+++ actions/authentication.js
@@ -1,36 +1,9 @@
-/* global fetch */
 import { Constants, SecureStore } from 'expo';
 import Sentry from 'sentry-expo';
 import { signIn, signOut } from '../apis';
+import refetch from '../utils/refetch';
 
 const { serviceUrl } = Constants.manifest.extra;
-
-async function refetch(url, options) {
-  const params = Object.assign({ headers: {} }, options);
-
-  params.headers['content-type'] = 'application/json';
-
-  const response = await fetch(url, params);
-
-  let values;
-
-  try {
-    if (response.status !== 204) {
-      values = await response.json();
-    }
-  } catch (e) {
-    values = {};
-  }
-
-  if (!response.ok) {
-    const error = new Error(`Response was not okay when calling: ${url}`);
-    error.status = response.status;
-    error.reason = values.error || values.message || 'unknown';
-    throw error;
-  }
-
-  return values;
-}
 
 async function save(values) {
   return SecureStore.setItemAsync('auth', JSON.stringify(values));

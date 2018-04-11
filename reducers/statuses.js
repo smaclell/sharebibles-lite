7569b33ff8ee1b059554a5e375277fe531e80e9d
Scott MacLellan <scott.maclellan@vidyard.com>
2018-04-10 22:47:12 -0400

    Tweaking the colours like rob suggested

--- reducers/statuses.js
+++ reducers/statuses.js
@@ -2,13 +2,13 @@
 const statuses = [
   // Home
   { key: 'accepted', label: 'status/accepted', icon: 'book', iconFamily: 'font-awesome', pinColor: 'green', group: 'home', success: true },
-  { key: 'need', label: 'status/need', icon: 'exclamation-circle', iconFamily: 'font-awesome', pinColor: 'grey', group: 'home', success: false },
+  { key: 'need', label: 'status/need', icon: 'exclamation-circle', iconFamily: 'font-awesome', pinColor: 'blue', group: 'home', success: false },
   { key: 'have', label: 'status/have', icon: 'check-circle', iconFamily: 'font-awesome', pinColor: 'green', group: 'home', success: true },
-  { key: 'rejected', label: 'status/rejected', icon: 'circle-with-cross', iconFamily: 'entypo', pinColor: 'grey', group: 'home', success: false },
+  { key: 'rejected', label: 'status/rejected', icon: 'circle-with-cross', iconFamily: 'entypo', pinColor: 'red', group: 'home', success: false },
 
   // Not Home
   { key: 'delivered', label: 'status/delivered', icon: 'book', iconFamily: 'font-awesome', pinColor: 'green', group: 'not_home', success: true },
-  { key: 'unfinished', label: 'status/unfinished', icon: 'block', iconFamily: 'entypo', pinColor: 'grey', group: 'not_home', success: false },
+  { key: 'unfinished', label: 'status/unfinished', icon: 'block', iconFamily: 'entypo', pinColor: 'blue', group: 'not_home', success: false },
 ];
 
 function reducer(state = statuses) {

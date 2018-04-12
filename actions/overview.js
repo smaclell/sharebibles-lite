e85c2404e15871aa19b0abb780e2657e3f862862
Justin Fangrad <justin.fangrad@gmail.com>
2018-04-11 21:07:22 -0400

    Unused vars

--- actions/overview.js
+++ actions/overview.js
@@ -1,24 +1,24 @@
 import * as apis from '../apis';
-import { fetchAllLocationData } from './locations';
-import { wrapLatitude, wrapLongitude } from '../utils/geo';
+// import { fetchAllLocationData } from './locations';
+// import { wrapLatitude, wrapLongitude } from '../utils/geo';
 
 const REGION_MODE = apis.GEO_REGION_KEY;
 
-const initial = {
-  updateCriteria() {},
-  cancel() {},
-};
-let query = initial;
+// const initial = {
+//   updateCriteria() {},
+//   cancel() {},
+// };
+// let query = initial;
 
-function getGeoKey() {
-  return (dispatch, getState) => {
-    const { authentication: { regionKey } } = getState();
-    if (!regionKey) {
-      return null;
-    }
-    return `${REGION_MODE}/${regionKey}`;
-  };
-}
+// function getGeoKey() {
+//   return (dispatch, getState) => {
+//     const { authentication: { regionKey } } = getState();
+//     if (!regionKey) {
+//       return null;
+//     }
+//     return `${REGION_MODE}/${regionKey}`;
+//   };
+// }
 
 // function updateLocations() {
 //   return (dispatch, getState) => {

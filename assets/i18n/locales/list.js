50b7ef55642832148077ee5b8617ca4d1a6935fd
Rob Wiebe <rjw.ywam@gmail.com>
2018-07-16 13:52:40 -1000

    Adding Portuguese/updating list.js

--- assets/i18n/locales/list.js
+++ assets/i18n/locales/list.js
@@ -3,4 +3,8 @@ export default {
   'locale/en': 'English',
   'locale/es': 'Español',
   'locale/th': 'ภาษาไทย',
+  'locale/de': 'Deutsche',
+  'locale/ko': '한국어',
+  'locale/pt': 'Português',
+  'locale/zh': '中文',
 };
---
+++ assets/i18n/locales/pt.js
@@ -0,0 +1,4 @@
+import 'moment/locale/pt';
+import * as translations from './pt.json';
+
+export default translations;

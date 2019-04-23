fb65045119f7473876ee083d5df165675e1aef30
Scott MacLellan <scott.maclellan@vidyard.com>
2019-04-23 09:27:01 -0400

    Adding the missing preset to fix the build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-exp publish --release-channel prod-v32 --non-interactive
-exp build:android --release-channel prod-v32 --non-interactive --no-publish
-exp build:ios --release-channel prod-v32 --non-interactive --no-publish
+expo publish --release-channel prod-v32 --non-interactive
 
-curl -o /personal/lite-secrets/releases/sb.1.3.0.apk "$(exp url:apk --non-interactive)"
-curl -o /personal/lite-secrets/releases/sb.1.3.0.ipa "$(exp url:ipa --non-interactive)"
+expo build:android --release-channel prod-v32 --non-interactive --no-publish
+curl -o /personal/lite-secrets/releases/sb.1.3.0.apk "$(expo url:apk --non-interactive)"
+
+# On the next build use --clear-push-cert
+expo build:ios --release-channel prod-v32 --non-interactive --no-publish
+curl -o /personal/lite-secrets/releases/sb.1.3.0.ipa "$(expo url:ipa --non-interactive)"
 

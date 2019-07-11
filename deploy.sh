85f0ec42f204bd28a74508fb8c69b6d3f3619f95
Scott MacLellan <scott.maclellan@vidyard.com>
2019-07-10 22:43:14 -0400

    Update the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-expo publish --release-channel prod-v32 --non-interactive
+expo publish --release-channel prod-v33 --non-interactive
 
-expo build:android --release-channel prod-v32 --non-interactive --no-publish
-curl -o /personal/lite-secrets/releases/sb.1.3.0.apk "$(expo url:apk --non-interactive)"
+expo build:android --release-channel prod-v33 --non-interactive --no-publish
+curl -o /personal/lite-secrets/releases/sb.1.3.1.apk "$(expo url:apk --non-interactive)"
 
 # On the next build use --clear-push-cert
-expo build:ios --release-channel prod-v32 --non-interactive --no-publish
-curl -o /personal/lite-secrets/releases/sb.1.3.0.ipa "$(expo url:ipa --non-interactive)"
+expo build:ios --release-channel prod-v33 --non-interactive --no-publish
+curl -o /personal/lite-secrets/releases/sb.1.3.1.ipa "$(expo url:ipa --non-interactive)"
 

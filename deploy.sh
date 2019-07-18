be5e8f4cedd907ee26da8e8c6896e03dd361284c
Scott MacLellan <scott.maclellan@vidyard.com>
2019-07-18 19:38:46 -0400

    Extending the deploy commands

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 expo publish --release-channel prod-v33 --non-interactive
 
-expo build:android --release-channel prod-v33 --non-interactive --no-publish
+expo build:android --release-channel prod-v33 --non-interactive --no-publish -t app-bundle
 curl -o /personal/lite-secrets/releases/sb.1.3.1.apk "$(expo url:apk --non-interactive)"
+expo upload:android
 
 # On the next build use --clear-push-cert
+# I have also never tried expo upload:ios, but it sounds like fun
 expo build:ios --release-channel prod-v33 --non-interactive --no-publish
 curl -o /personal/lite-secrets/releases/sb.1.3.1.ipa "$(expo url:ipa --non-interactive)"
+expo upload:ios
 

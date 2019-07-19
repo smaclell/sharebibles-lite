57d21f2e649404411d0bd1ba4f2819896a526956
Scott MacLellan <scott.maclellan@vidyard.com>
2019-07-18 22:18:09 -0400

    Correcting the extension in for the android file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 expo publish --release-channel prod-v33 --non-interactive
 
 expo build:android --release-channel prod-v33 --non-interactive --no-publish -t app-bundle
-curl -o /personal/lite-secrets/releases/sb.1.3.1.apk "$(expo url:apk --non-interactive)"
+curl -o /personal/lite-secrets/releases/sb.1.3.1.aab "$(expo url:apk --non-interactive)"
 expo upload:android
 
 # On the next build use --clear-push-cert
